4bcb3466a1770c9d0fe7f53e5a63fa37a97c8091
priebepaloma-lab <priebepaloma@gmail.com>
2025-10-24 22:55:34 -0300

    fix: resolved next.config.ts conflict

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-<<<<<<< HEAD
   reactCompiler: true,
-=======
->>>>>>> 0edce9270c3332c42d0ddadaf5aa199cc416f7e7
 };
 
 export default nextConfig;
